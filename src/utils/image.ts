887a0a66b679554795469b9b97d5911845405735
viper-00 <zhongmingyang2000@gmail.com>
2025-08-19 00:09:38 +0800

    update image of product type

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -5,33 +5,33 @@ export function GetImgSrcByProductType(type: string): string {
 
   switch (type) {
     case PRODUCT_TYPE.WOMEN:
-      return baseUrl + '/images/product/women.png';
+      return baseUrl + '/images/product/women.jpg';
     case PRODUCT_TYPE.MEN:
-      return baseUrl + '/images/product/men.png';
+      return baseUrl + '/images/product/men.jpg';
     case PRODUCT_TYPE.BEAUTY:
-      return baseUrl + '/images/product/beauty.png';
+      return baseUrl + '/images/product/beauty.jpg';
     case PRODUCT_TYPE.FOOD:
-      return baseUrl + '/images/product/food.png';
+      return baseUrl + '/images/product/food.jpg';
     case PRODUCT_TYPE.BABY:
-      return baseUrl + '/images/product/baby.png';
+      return baseUrl + '/images/product/baby.jpg';
     case PRODUCT_TYPE.HOME:
-      return baseUrl + '/images/product/home.png';
+      return baseUrl + '/images/product/home.jpg';
     case PRODUCT_TYPE.FITNESS:
-      return baseUrl + '/images/product/fitness.png';
+      return baseUrl + '/images/product/fitness.jpg';
     case PRODUCT_TYPE.ACCESSORIES:
-      return baseUrl + '/images/product/accessories.png';
+      return baseUrl + '/images/product/accessories.jpg';
     case PRODUCT_TYPE.PET:
-      return baseUrl + '/images/product/pet.png';
+      return baseUrl + '/images/product/pet.jpg';
     case PRODUCT_TYPE.GAMES:
-      return baseUrl + '/images/product/games.png';
+      return baseUrl + '/images/product/games.jpg';
     case PRODUCT_TYPE.ELECTRONICS:
-      return baseUrl + '/images/product/electronics.png';
+      return baseUrl + '/images/product/electronics.jpg';
     case PRODUCT_TYPE.ARTS:
-      return baseUrl + '/images/product/arts.png';
+      return baseUrl + '/images/product/arts.jpg';
     case PRODUCT_TYPE.LUGGAGE:
-      return baseUrl + '/images/product/luggage.png';
+      return baseUrl + '/images/product/luggage.jpg';
     case PRODUCT_TYPE.SPORTING:
-      return baseUrl + '/images/product/sporting.png';
+      return baseUrl + '/images/product/sporting.jpg';
     default:
       return '';
   }
