c9d28101191f1bec41c9e4cc188b6c078caea0bb
viper-00 <zhongmingyang2000@gmail.com>
2026-07-20 21:53:49 +0800

    update

--- src/packages/constants/general.ts
+++ src/packages/constants/general.ts
@@ -3,7 +3,5 @@ export const APP_DESCRIPTION = 'Decentralized Digital Exchange Platform'
 export const ENV = process.env.NEXT_PUBLIC_ENVIRONMENT ?? 'development'
 export const IS_DEVELOPMENT = process.env.NEXT_PUBLIC_ENVIRONMENT === 'development'
 export const IS_PRODUCATION = !IS_DEVELOPMENT
-
-export const STATIC_ASSETS = ''
 export const WALLETCONNECT_PROJECT_ID =
   process.env.WALLETCONNECT_PROJECT_ID || 'a3237b68315df229bdc08d45fc039e6b'
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { APP_NAME, STATIC_ASSETS } from '@/packages/constants';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { APP_NAME } from '@/packages/constants'
+import { Http } from '@/utils/http/http'
 
 export default function MyDocument(props: any) {
   return (
@@ -8,15 +9,10 @@ export default function MyDocument(props: any) {
         {/* <DocumentHeadTags {...props} /> */}
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        {/* <link
-          rel="icon"
-          href={`${STATIC_ASSETS}/brand/logo.svg`}
-          type="image/svg+xml"
-        /> */}
-        <link rel="icon" href={`${STATIC_ASSETS}/favicon.ico`} type="image/x-icon" />
-        <link rel="shortcut icon" href={`${STATIC_ASSETS}/favicon.ico`} />
+        <link rel="icon" href={`${Http.httpClient}/favicon.ico`} type="image/x-icon" />
+        <link rel="shortcut icon" href={`${Http.httpClient}/favicon.ico`} />
         <meta name="theme-color" content="#ffffff" />
-        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link rel="manifest" href={`${Http.httpClient}/manifest.json`} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
@@ -27,7 +23,7 @@ export default function MyDocument(props: any) {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
 
 // MyDocument.getInitialProps = async (ctx: any) => {
